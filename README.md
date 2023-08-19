6bc660666212f6a52b54a929369dfe7c1bb733df
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-08-19 18:44:32 +0300

    add general configs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 let Django publish static files, In production NGINX
+
+## Tools
+* black -> pip install black, black views.py
+* flake8 -> pip install flake8, flake8 views.py
--- core/settings/base.py
+++ core/settings/base.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dotenv
+import os
 from pathlib import Path
+
+dotenv.read_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4yhv*+)otu387820u@x)kts4$k%&evk&_j53^2q$#hmwte_wsc"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 ALLOWED_HOSTS = []
