461ab7e6c3e2eff1fc9382d91b413fa061a0ab15
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-08-29 13:59:48 +0300

    add tag apiview

--- apps/tags/api/views.py
+++ apps/tags/api/views.py
@@ -1,3 +1,7 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from apps.tags.models import Tag
 from rest_framework.generics import get_object_or_404, ListAPIView
 
@@ -6,14 +10,60 @@ from apps.tags.api.pagination import TagPagination
 from apps.links.api.serializers import SocialNetworkSerializer
 
 
-class TagView(ListAPIView):
-    serializer_class = TagSerializer
-    queryset = Tag.objects.all()
+# class TagView(ListAPIView):
+#     serializer_class = TagSerializer
+#     queryset = Tag.objects.all()
+#
+#     def get_queryset(self):
+#         return Tag.objects.filter(status=True)
 
-    def get_queryset(self):
-        return Tag.objects.filter(status=True)
+
+class TagView(APIView):
+
+    def post(self, request):
+        serializer = TagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "status": "success",
+                    "data": serializer.data
+                },
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {
+                    "status": "error",
+                    "data": serializer.errors
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def get(self, request, id=None):
+        if id:
+            item = Tag.objects.get(id=id, status=True)
+            serializer = TagSerializer(item)
+            return Response(
+                {
+                    "status": "success",
+                    "data": serializer.data
+                },
+                status=status.HTTP_200_OK
+            )
+
+        items = Tag.objects.filter(status=True)
+        serializer = TagSerializer(items, many=True)
+        return Response(
+            {
+                "status": "success",
+                "data": serializer.data
+            },
+            status=status.HTTP_200_OK
+        )
 
 
+# /list?q=12, tags/search?name=software
 class TagQueryParamsView(ListAPIView):
     serializer_class = TagSerializer
     # pagination_class = TagPagination
