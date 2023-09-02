2fa88a5667ccdaa36d9356d9bfa9082dc64dbe2e
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-09-02 13:40:31 +0300

    upd logout views

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -6,6 +6,8 @@ from rest_framework.decorators import permission_classes, api_view
 from rest_framework.generics import RetrieveUpdateAPIView, get_object_or_404, CreateAPIView, UpdateAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 
 from rest_framework.views import APIView
 
@@ -72,14 +74,38 @@ class UpdateProfileView(UpdateAPIView):
 
 
 class  LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
 
-    def get(self, request):
+    def post(self, request):
         try:
-            request.user.auth_token.delete()
-            logout(request)
-            return redirect('home')
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutAllView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    # def post(self, request):
+    #     tokens = OutstandingToken.objects.filter(user_id=request.user.id)
+    #     for token in tokens:
+    #         t, _ = BlacklistedToken.objects.get_or_create(token=token)
+    #
+    #     return Response(status=status.HTTP_205_RESET_CONTENT)
+
+    def post(self, request, *args, **kwargs):
+        if self.request.data.get('all'):
+            token: OutstandingToken
+            for token in OutstandingToken.objects.filter(user=request.user):
+                _, _ = BlacklistedToken.objects.get_or_create(token=token)
+            return Response({"status": "OK, goodbye, all refresh tokens blacklisted"})
+        refresh_token = self.request.data.get('refresh_token')
+        token = RefreshToken(token=refresh_token)
+        token.blacklist()
+        return Response({"status": "OK, goodbye"})
 
 
 # @api_view(['POST'])
