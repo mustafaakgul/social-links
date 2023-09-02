f8912f7fd03ceb429f312f0fe42cf2910af88f1f
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-09-02 13:39:03 +0300

    upd permissions

--- apps/accounts/api/permissions.py
+++ apps/accounts/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
 class NotAuthenticated(BasePermission):
@@ -6,3 +7,14 @@ class NotAuthenticated(BasePermission):
 
     def has_permission(self, request, view):
         return not request.user.is_authenticated
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
