4b8e21df44c3f346c77483f19739280795b592f0
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-09-02 13:41:13 +0300

    add custom response model and better exception man.

--- apps/tags/api/views.py
+++ apps/tags/api/views.py
@@ -21,21 +21,34 @@ from apps.links.api.serializers import SocialNetworkSerializer
 class TagView(APIView):
 
     def post(self, request):
-        serializer = TagSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                {
-                    "status": "success",
-                    "data": serializer.data
-                },
-                status=status.HTTP_200_OK
-            )
-        else:
+        try:
+            serializer = TagSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "status": "success",
+                        "message": "Tag created successfully",
+                        "errors": None,
+                        "data": serializer.data
+                    },
+                    status=status.HTTP_200_OK
+                )
+            else:
+                return Response(
+                    {
+                        "status": "error",
+                        "data": serializer.errors
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        except Exception as e:
             return Response(
                 {
                     "status": "error",
-                    "data": serializer.errors
+                    "message": "An error occurred",
+                    "errors": e.args,
+                    "data": str(e)
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
