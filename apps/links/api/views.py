679e8f8e5914602f85074af7f43d301f7387172e
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-12-09 00:59:28 +0300

    add custom link views

--- apps/links/api/views.py
+++ apps/links/api/views.py
@@ -1,7 +1,9 @@
 from apps.tags.models import Tag
+from apps.links.models import CustomLink
 from rest_framework.generics import get_object_or_404, ListAPIView
 
 from apps.tags.api.serializers import TagSerializer
+from apps.links.api.serializers import CustomLinkSerializer
 
 
 class TagView(ListAPIView):
@@ -11,3 +13,12 @@ class TagView(ListAPIView):
 
     def get_queryset(self):
         return Tag.objects.filter(status=True)
+
+
+class ListCustomLinkView(ListAPIView):
+    serializer_class = CustomLinkSerializer
+    queryset = CustomLink.objects.all()
+    ordering_fields = ['created_at']
+
+    def get_queryset(self):
+        return CustomLink.objects.filter(user=self.request.user)
