c9329195999f4e63bf6738bc57cd37fe3d96e641
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-12-08 23:49:30 +0300

    upd base

--- core/settings/local.py
+++ core/settings/local.py
@@ -16,3 +16,11 @@ DATABASES = {
 
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "nodeme-backend-env.eba-6zp3pfnk.eu-central-1.elasticbeanstalk.com", '*']
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True
+}
