63495e37ddee5f391484e6ced15dd5d2c888b1e2
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-12-09 00:59:15 +0300

    add custom link url

--- apps/links/api/urls.py
+++ apps/links/api/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from apps.tags.api.views import (
                         TagView,
                         )
+from apps.links.api.views import (
+                        ListCustomLinkView,
+                        )
 
 
 app_name = "links"
 
 
 urlpatterns = [
-    path('', TagView.as_view(), name='list'),
+    path('tags', TagView.as_view(), name='list'),
+    path('custom-links', ListCustomLinkView.as_view(), name='custom-link-list'),
 ]
